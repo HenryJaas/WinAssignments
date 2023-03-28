25eddd366db4a461830d98602f694504b6242b96
HenryJaas <henry.jaas31@gmail.com>
2023-03-28 03:11:25 +0300

    assignment4

---
+++ Assignment4/Assignment4/Flight.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment4
+{
+    public class Flight
+    {
+        int _id;
+        string _origin;
+        string _destination;
+        DateTime _date;
+        double _price;
+
+        public Flight(int id, string origin, string destination, DateTime date, double price)
+        {
+            _id = id;
+            _origin = origin;
+            _destination = destination;
+            _date = date;
+            _price = price;
+        }
+
+        public int Id
+        {
+            get { return _id; }
+        }
+
+        public string Origin
+        {
+            get { return _origin; }
+            set { _origin = value; }
+        }
+
+        public string Destination
+        {
+            get { return _destination; }
+            set { _destination = value; }
+        }
+
+        public DateTime Date
+        {
+            get { return _date; }
+            set { _date = value; }
+        }
+
+        public double Price
+        {
+            get { return _price; }
+            set { _price = value; }
+        }
+    }
+}
---
+++ Testit/Testit/AirlineCompany.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestiKpl2
+{
+    public class AirlineCompany
+    {
+        
+         readonly string _airlineName;      
+        SortedList<Flight, double> _flights = new SortedList<Flight, double>(new FlightComparer());
+       
+        public AirlineCompany(string airlineName)
+        {
+            _airlineName = airlineName;
+        }
+
+        public void AddFlight(Flight flight)
+        {
+            _flights.Add(flight, flight.Price);
+        }
+
+        public void DeleteFlight(Flight flight)
+        {
+            _flights.Remove(flight);
+        }
+
+        public void EditFlight(Flight flight, double newPrice)
+        {
+            _flights.Remove(flight);
+            flight.Price = newPrice;
+            _flights.Add(flight, newPrice);
+        }
+
+        public Flight FindFlight(int id)
+        {
+            foreach (Flight flight in _flights.Keys)
+            {
+                if (flight.Id == id)
+                {
+                    return flight;
+                }
+            }
+
+            return null;
+        }
+
+        public Flight GetCheapestFlight()
+        {
+            return _flights.Keys[0];
+        }
+
+        public Flight GetMostExpensiveFlight()
+        {
+            return _flights.Keys[_flights.Count - 1];
+        }
+
+        public int Count
+        {
+            get { return _flights.Count; }
+        }
+
+        public Flight this[int index]
+        {
+            get { return _flights.Keys[index]; }
+            set { _flights.Keys[index] = value; }
+        }
+
+        private class FlightComparer : IComparer<Flight>
+        {
+            public int Compare(Flight x, Flight y)
+            {
+                return x.Price.CompareTo(y.Price);
+            }
+        }
+    }
+}
